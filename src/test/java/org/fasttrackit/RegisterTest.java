7b9329c178734737f2a6d123c4a7844b7808927e
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2018-11-14 15:00:12 +0200

    practiced basic web page interactions using Selenium WebDriver

---
+++ src/test/java/org/fasttrackit/RegisterTest.java
@@ -0,0 +1,28 @@
+package org.fasttrackit;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class RegisterTest {
+
+    @Test
+    public void successfulRegisterWithAllDetails() {
+        System.setProperty("webdriver.chrome.driver", "C:\\webdrivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://fasttrackit.org/selenium-test");
+
+        driver.findElement(By.linkText("ACCOUNT")).click();
+        driver.findElement(By.linkText("Register")).click();
+
+        driver.findElement(By.id("firstname")).sendKeys("Flaviu");
+        driver.findElement(By.id("middlename")).sendKeys("G");
+        driver.findElement(By.id("lastname")).sendKeys("Ratiu");
+        driver.findElement(By.name("email")).sendKeys("test@example.com");
+        driver.findElement(By.name("password")).sendKeys("passw0rd");
+        driver.findElement(By.name("confirmation")).sendKeys("passw0rd");
+        driver.findElement(By.className("checkbox")).click();
+    }
+}
